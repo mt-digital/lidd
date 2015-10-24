b3c1a9a36fb938df645ffe88e1a3dcff051eeb15
Matthew Turner <maturner01@gmail.com>
2015-10-23 22:18:34 -0700

    creating db by name, collection with class name, in mongo

---
+++ src/main/java/hello/ApplicationConfig.java
@@ -0,0 +1,27 @@
+package hello;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import com.mongodb.Mongo;
+import com.mongodb.WriteConcern;
+
+@Configuration
+@ComponentScan
+@EnableMongoRepositories
+class ApplicationConfig extends AbstractMongoConfiguration {
+
+  @Override
+  protected String getDatabaseName() {
+    return "e-store";
+  }
+
+  @Override
+  public Mongo mongo() throws Exception {
+    Mongo mongo = new Mongo();
+    mongo.setWriteConcern(WriteConcern.SAFE);
+    return mongo;
+  }
+}
